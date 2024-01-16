03bf00ae22d7314511a381b88510935fecb2db05
YIJING WU <yijingwu@YIJINGdeMacBook-Pro.local>
2024-01-16 14:51:10 -0800

    added a TextInput and changeTextHandler function.

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 import Header from "./components/Header";
+import { useState } from "react";
 
 export default function App() {
   const appName = "my first app";
+  const [text, setText] = useState("");
+
+  // callback handler
+  function changeTextHandler(changedText) {
+    setText(changedText);
+  }
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Header name={appName} version={2} />
+      <TextInput
+        placeholder="Type something"
+        style={styles.input}
+        value={text}
+        onChangeText={changeTextHandler}
+      />
+      <Text>{text}</Text>
     </View>
   );
 }
@@ -18,5 +32,10 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  input: {
+    borderBottomWidth: 2,
+    borderBottomColor: "purple",
+    width: "50%",
   },
 });

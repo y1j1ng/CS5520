81cd8b7dd0898d980aed16940435e8da4ff486f7
YIJING WU <yijingwu@YIJINGdeMacBook-Pro.local>
2024-03-19 13:54:39 -0700

    added the securtiy rules in firebase and updated the snapshot listener

--- components/Home.js
+++ components/Home.js
@@ -15,16 +15,20 @@ import { useState, useEffect } from "react";
 import Input from "./Input";
 import GoalItem from "./GoalItem";
 import PressableButton from "./PressableButton";
-import { collection, onSnapshot } from "firebase/firestore";
-
+import { collection, onSnapshot, query, where } from "firebase/firestore";
 import { deleteFromDB, writeToDB } from "../firebase-files/firestoreHelper";
-import { database } from "../firebase-files/firebaseSetup";
+import { database, auth } from "../firebase-files/firebaseSetup";
+
 export default function Home({ navigation }) {
   function cleanup() {}
   useEffect(() => {
     // set up a listener to get realtime data from firestore - only after the first render
-    const unsubscribe = onSnapshot(
+    const q = query(
       collection(database, "goals"),
+      where("owner", "==", auth.currentUser.uid)
+    );
+    const unsubscribe = onSnapshot(
+      q,
       (querySnapshot) => {
         if (querySnapshot.empty) {
           Alert.alert("You need to add something");
@@ -41,6 +45,9 @@ export default function Home({ navigation }) {
         // console.log(newArray);
         //updating the goals array with the new array
         setGoals(newArray);
+      },
+      (error) => {
+        Alert.alert(error.message);
       }
     );
     return () => {
--- firebase-files/firestoreHelper.js
+++ firebase-files/firestoreHelper.js
@@ -1,11 +1,14 @@
 import { collection, addDoc, doc, deleteDoc, getDoc } from "firebase/firestore";
-import { database } from "./firebaseSetup";
+import { database, auth } from "./firebaseSetup";
 
 export async function writeToDB(data, col, docId, subCol) {
   try {
     if (docId) {
       await addDoc(collection(database, col, docId, subCol), data);
     } else {
+      if (col === "goals") {
+        data = { ...data, owner: auth.currentUser.uid };
+      }
       await addDoc(collection(database, col), data);
     }
   } catch (error) {
